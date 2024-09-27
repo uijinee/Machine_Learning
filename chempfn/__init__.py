0d221d2e35a901f0ba76dcbd45efc1dd9ea7700e
uijinee <abrahamkim98@gmail.com>
2024-09-27 17:25:42 +0900

    pfn

---
+++ chempfn/__init__.py
@@ -0,0 +1,64 @@
+from typing import Optional
+import time
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, auc
+import numpy as np
+from tqdm import tqdm
+from eosce.models import ErsiliaCompoundEmbeddings
+
+from .ensemble_tabpfn import EnsembleTabPFN
+
+
+class ChemPFN:
+    def __init__(
+        self,
+        max_iters: int = 10,
+        random_state: Optional[int] = None,
+        early_stopping_rounds: int = 5,
+        tolerance: float = 1e-2,
+        verbose: bool = False,
+    ) -> None:
+        self.etpfn = EnsembleTabPFN(
+            max_iters=max_iters,
+            random_state=random_state,
+            early_stopping_rounds=early_stopping_rounds,
+            tolerance=tolerance,
+            verbose=verbose,
+            baseline=False,
+        )
+        self.eosce = ErsiliaCompoundEmbeddings()
+
+    def fit(self, smiles_list, y):
+        self.etpfn.fit(self.eosce.transform(smiles_list), y)
+
+    def predict(self, smiles_list):
+        return self.etpfn.predict(self.eosce.transform(smiles_list))
+
+    def predict_proba(self, smiles_list):
+        return self.etpfn.predict_proba(self.eosce.transform(smiles_list))
+
+    def evaluate(self, smiles_list, y):
+        data = {}
+        data["n_pos"] = int(np.sum(y))
+        data["n_neg"] = len(y) - np.sum(y)
+        splitter = StratifiedKFold(shuffle=True, random_state=42, n_splits=5)
+        aurocs = []
+        t0 = time.time()
+        for train_idx, test_idx in tqdm(
+            splitter.split([i for i in range(len(smiles_list))], y)
+        ):
+            train_smiles = [smiles_list[idx] for idx in train_idx]
+            test_smiles = [smiles_list[idx] for idx in test_idx]
+            train_y = [y[idx] for idx in train_idx]
+            test_y = [y[idx] for idx in test_idx]
+            self.fit(train_smiles, train_y)
+            y_hat = self.predict_proba(test_smiles)[:, 1]
+            fpr, tpr, _ = roc_curve(test_y, y_hat)
+            auroc = auc(fpr, tpr)
+            aurocs += [auroc]
+        t1 = time.time()
+        data["auroc_mean"] = np.mean(aurocs)
+        data["auroc_std"] = np.std(aurocs)
+        data["time_elapsed_sec"] = t1 - t0
+        return data
---
+++ chempfn/utils.py
@@ -0,0 +1,144 @@
+import enum
+from urllib.error import HTTPError
+import pandas as pd
+from typing import Optional
+
+
+class TabPFNConstants(enum.IntEnum):
+    """Constants mapping TabPFN's limitations."""
+
+    MAX_INP_SIZE: int = 1000
+    MAX_FEAT_SIZE: int = 100
+
+
+class AntiMicrobialsDatasetCutoff(enum.Enum):
+    LOW = "lc"
+    HIGH = "hc"
+
+
+ANTI_MICROBIALS_DATASET_TYPES = {
+    "organism all": "org_all",
+    "mic": "MIC",
+    "activity": "Activity",
+    "all": "all",
+    "ic50": "IC50",
+    "iz": "IZ",
+    "inhibition": "Inhibition",
+    "organism": "org",
+    "protein": "prot",
+    "protein all": "prot_all",
+}
+
+TOP_N_ASSAYS = {"0": "top_0", "1": "top_1", "2": "top_2", "": ""}
+
+ANTIMICROBIAL_PATHOGENS = {
+    "acinetobacter baumannii": "abaumannii",
+    "campylobacter spp.": "campylobacter",
+    "enterococcus faecium": "efaecium",
+    "enterobacter spp.": "enterobacter",
+    "escherichia coli": "ecoli",
+    "helicobacter pylori": "hpylori",
+    "klebsiella pneumoniae": "kpneumoniae",
+    "mycobacterium tuberculosis": "mtuberculosis",
+    "neisseria gonorrhoeae": "ngonorrhoeae",
+    "plasmodium spp.": "pfalciparum",
+    "pseudomonas aeruginosa": "paeruginosa",
+    "schistosoma mansoni": "smansoni",
+    "staphylococcus aureus": "saureus",
+    "streptococcus pneumoniae": "spneumoniae",
+}
+
+
+class AntiMicrobialsDataLoader:
+    """Class to load the AntiMicrobial dataset from S3."""
+
+    def __init__(self) -> None:
+        self.base_url = "https://chempfn-data.s3.eu-central-1.amazonaws.com"
+        self.folder = "data/new_processing"
+
+    def _check_dataset_type(self, dataset_type: str) -> str:
+        if dataset_type in ANTI_MICROBIALS_DATASET_TYPES:
+            return ANTI_MICROBIALS_DATASET_TYPES[dataset_type]
+        raise ValueError(f"Invalid dataset_type: {dataset_type}")
+
+    def _check_pathogen(self, pathogen: str) -> str:
+        if pathogen in ANTIMICROBIAL_PATHOGENS:
+            return ANTIMICROBIAL_PATHOGENS[pathogen]
+        if pathogen in ANTIMICROBIAL_PATHOGENS.values():
+            return pathogen
+        raise ValueError(f"Invalid pathogen: {pathogen}")
+
+    def _check_cutoff(self, cutoff: str) -> str:
+        if cutoff in [e.value for e in AntiMicrobialsDatasetCutoff]:
+            return cutoff
+        else:
+            raise ValueError(f"Invalid cutoff: {cutoff}")
+
+    def _check_top_n_assays(self, num: str) -> str:
+        if num in TOP_N_ASSAYS:
+            return TOP_N_ASSAYS[num]
+        else:
+            raise ValueError(f"Invalid top_n_assays: {num}")
+
+    def _validate_input(
+        self, pathogen: str, dataset_type: str, cutoff: str, assay_num: str
+    ) -> tuple:
+        pathogen = self._check_pathogen(pathogen.lower())
+        dataset_type = self._check_dataset_type(dataset_type.lower())
+        cutoff = self._check_cutoff(cutoff.lower())
+        assay_num = self._check_top_n_assays(assay_num)
+        return (pathogen, cutoff, dataset_type, assay_num)
+
+    def _build_url_from_input(
+        self, pathogen: str, dataset_type: str, cutoff: str, assay_num: str
+    ) -> str:
+        _pathogen, _cutoff, _dataset_type, _assay_num = self._validate_input(
+            pathogen, dataset_type, cutoff, assay_num
+        )
+        if _assay_num:
+            url = f"{self.base_url}/{self.folder}/{_pathogen}/{_pathogen}_{_dataset_type}_{_cutoff}_{_assay_num}.csv"
+        else:
+            url = f"{self.base_url}/{self.folder}/{_pathogen}/{_pathogen}_{_dataset_type}_{_cutoff}.csv"
+        return url
+
+    def load(
+        self,
+        pathogen: str,
+        cutoff: str = AntiMicrobialsDatasetCutoff.HIGH.value,
+        dataset_type: str = "organism",
+        assay_num: str = "",
+        fully_qualified: str = ""
+    ) -> Optional[pd.DataFrame]:
+        """Load the AntiMicrobial dataset from S3.
+
+        Parameters
+        ----------
+        pathogen : str
+            Pathogen name.
+        dataset_type : str
+            One of ANTI_MICROBIALS_DATASET_TYPES
+        cutoff : str
+            One of AntiMicrobialsDatasetCutoff
+
+        Returns
+        -------
+        pd.DataFrame
+            AntiMicrobial dataset.
+        """
+        _pathogen, _cutoff, _dataset_type, _assay_num = self._validate_input(
+            pathogen, dataset_type, cutoff, assay_num
+        )
+        if fully_qualified:
+            url = f"{self.base_url}/{self.folder}/{_pathogen}/{fully_qualified}.csv"
+
+        else:
+            url = self._build_url_from_input(
+                pathogen, dataset_type, cutoff, assay_num
+            )
+            
+        try:
+            df = pd.read_csv(url)
+        except HTTPError:
+                print(f"This dataset is not available for the pathogen: {pathogen}")
+                return None
+        return df
